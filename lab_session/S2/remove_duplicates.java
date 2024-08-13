f47a71b6ed4c0f9e429e618f53a9cb68677b18d5
lkx100 <lk5999950@gmail.com>
2024-08-13 09:29:45 +0530

    better organisation

--- lab_session/S2/remove_duplicates.java
+++ lab_session/S2/remove_duplicates.java
@@ -1,13 +1,17 @@
 public class remove_duplicates {
     public static void main(String[] args) {
-        int[] arr = {0, 5, 3, 1, 0, 5, 9, 2, 1};
+        int[] arr = { 0, 5, 3, 1, 0, 5, 9, 2, 1 };
+        System.out.print("Original Array: ");
+        for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+        System.out.println();
         int[] unique = new int[arr.length];
         for (int i = 0; i < arr.length; i++) unique[i] = -1;
         int total_unique_elements = 0;
         for (int i = 0, index = 0; i < arr.length; i++) {
             boolean isUnique = true;
-            for (int j = i+1; j < arr.length; j++)
-                if (arr[j] == arr[i]) isUnique = false;
+            for (int j = i + 1; j < arr.length; j++) if (
+                arr[j] == arr[i]
+            ) isUnique = false;
             if (isUnique) {
                 total_unique_elements++;
                 unique[index++] = arr[i];
@@ -15,7 +19,6 @@ public class remove_duplicates {
         }
         System.out.println("Total Unique Elements: " + total_unique_elements);
         System.out.print("Unique Elements: ");
-        for (int i : unique)
-            if (i != -1) System.out.print(i + " ");
-    }    
+        for (int i : unique) if (i != -1) System.out.print(i + " ");
+    }
 }
